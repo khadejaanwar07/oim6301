c50e46843c653e32547d9e90c99e755e88e805e5
Khadeja Anwar <kanwar1@babson.edu>
2026-09-18 20:33:36 -0400

    Ordering Code

--- notebooks/01-first-notebook.py
+++ notebooks/01-first-notebook.py
@@ -290,6 +290,24 @@ def _(freight_charges):
     return
 
 
+@app.cell
+def _(freight_charges):
+    freight_charges[:3]
+    return
+
+
+@app.cell
+def _(orders):
+    orders[0] 
+    return
+
+
+@app.cell
+def _(freight_charges):
+    freight_charges[0]
+    return
+
+
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
